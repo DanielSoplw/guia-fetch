249c23b513cd20bbb246021bc03435a29508e435
DaniSoplw <danieleduardobautistap@gmail.com>
2026-09-13 23:24:14 -0500

    Reto 7 - Buscador de usuarios

--- js/app.js
+++ js/app.js
@@ -4,6 +4,8 @@
 })
 .catch(error => console.error(error));
 */
+
+let usuariosGlobales = [];
 const contenedor = document.querySelector("#contenedorUsuarios");
 
 function mostrarUsuarios(usuarios) {
@@ -28,24 +30,41 @@ function mostrarUsuarios(usuarios) {
 }
 
 async function cargarUsuarios() {
-    const mensaje = document.querySelector("#mensaje");
+  const mensaje = document.querySelector("#mensaje");
 
-    try {
+  try {
     mensaje.textContent = "Cargando usuarios...";
 
-    const response = await fetch( "https://jsonplaceholder.typicode.com/users"
+    const response = await fetch(
+      "https://jsonplaceholder.typicode.com/users"
     );
 
     if (!response.ok) {
-    throw new Error(`Error HTTP: ${response.status}`);
+      throw new Error(`Error HTTP: ${response.status}`);
     }
 
-    const usuarios = await response.json(); mostrarUsuarios(usuarios); mensaje.textContent = "";
-    } catch (error) {
+    usuariosGlobales = await response.json();
+    mostrarUsuarios(usuariosGlobales);
+    mensaje.textContent = "";
+  } catch (error) {
     mensaje.textContent = "No fue posible cargar la información.";
     console.error(error);
-    }
+  }
 }
+
+const buscar = document.querySelector("#buscar");
+
+buscar.addEventListener("input", () => {
+  const texto = buscar.value.toLowerCase();
+
+  const filtrados = usuariosGlobales.filter(usuario =>
+    usuario.name.toLowerCase().includes(texto) ||
+    usuario.email.toLowerCase().includes(texto) ||
+    usuario.address.city.toLowerCase().includes(texto)
+  );
+
+  mostrarUsuarios(filtrados);
+});
 
 
 cargarUsuarios();
